6d7e2fee0154ae2002da314d0b6358e63ff79c1c
Pierre <pierre.parientedimitrov@gmail.com>
2021-09-21 14:38:30 -0700

    created layout, added custom font

---
+++ components/Layouts/Layout.js
@@ -0,0 +1,56 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+const Layout = ({ children }) => {
+	return (
+		<div>
+			<Head>
+				<title>Aligned Agency</title>
+				<link rel='icon' href='/favicon.png' />
+				<link
+					rel='preload'
+					href='/public/font/PFAgoraSansPro-Black.ttf'
+					as='font'
+					crossOrigin=''
+				/>
+				<link
+					rel='preload'
+					href='/public/font/PFAgoraSansPro-Bold.ttf'
+					as='font'
+					crossOrigin=''
+				/>
+				<link
+					rel='preload'
+					href='/public/font/PFAgoraSansPro-Light.ttf'
+					as='font'
+					crossOrigin=''
+				/>
+				<link
+					rel='preload'
+					href='/public/font/PFAgoraSansPro-Medium.ttf'
+					as='font'
+					crossOrigin=''
+				/>
+				<link
+					rel='preload'
+					href='/public/font/PFAgoraSansPro-Regular.ttf'
+					as='font'
+					crossOrigin=''
+				/>
+				<link
+					rel='preload'
+					href='/public/font/PFAgoraSansPro-Thin.ttf'
+					as='font'
+					crossOrigin=''
+				/>
+			</Head>
+			<body>
+				<h1 className='font-thin'>Navbar</h1>
+				{children}
+				<h1>footer</h1>
+			</body>
+		</div>
+	);
+};
+
+export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,37 @@
+// tailwind.config.js
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	// mode: 'jit',
+	purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		colors: {
+			white: '#FFFFFF',
+			background: '#131313',
+			gray: {
+				DEFAULT: '#232323',
+				100: '#747474',
+				200: '#AFAFAF',
+			},
+			medieval: '#FCA188',
+			renaissance: '#FCC488',
+			baroque: '#FCEA88',
+			classical: '#86D3F6',
+			earlyRomantic: '#EF83B7',
+			romantic: '#EF8383',
+			lateRomantic: '#E25D7D',
+			modern: '#83EFDC',
+			contemporary: '#8583EF',
+		},
+		boxShadow: {
+			sm: '1px 2px 4px rgba(0, 0, 0, 0.11)',
+			md: '4px 8px 16px rgba(0, 0, 0, 0.1);',
+			inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+			none: 'none',
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
